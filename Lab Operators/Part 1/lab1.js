ab6bbf4806d5346f57467c112b15ac023706f6b4
hailealol <70154169+hailealol@users.noreply.github.com>
2020-12-02 12:49:42 -0500

    Add files via upload

---
+++ Lab Operators/Part 1/lab1.js
@@ -0,0 +1,17 @@
+var secondsInAMinute = 60;
+var minutesInAHour = 60;
+var hoursInADay = 24;
+var daysInAYear = 365;
+
+var secondsInAnHour = secondsInAMinute * minutesInAHour;
+document.write("The number of seconds in an hour is " + secondsInAnHour);
+document.write("<br>")
+
+var secondsInADay = secondsInAnHour * hoursInADay;
+document.write("The number of seconds in a day are " + secondsInADay);
+document.write("<br>")
+
+var secondsInAYear = secondsInADay * daysInAYear;
+document.write("The number of seconds in a year are " + secondsInAYear);
+document.write("<br>")
+document.write("Created by Hailea Reynolds")
